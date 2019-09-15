bb16128aa6c7b6c924e45bea0c822571cca0120d
Michal Wojtowicz <michal.wojtowicz@pb.com>
2019-09-15 12:51:13 +0200

    Added event "Aktywny wrzesien"

---
+++ src/data/aktywnyWrzesien.ts
@@ -0,0 +1,69 @@
+export default {
+    event: {
+        name: "aktywny-wrzesien",
+        title: "Aktywny Wrzesien 2019",
+        duration: {
+            from: "2019-09-01T00:00:00+0200",
+            to: "2019-09-30T23:59:59+0200"
+        },
+        desc: "Jesień w mieście jest piękna, doświadcz jej w z innymi mieszkańcami korzystając z przygotowanych dla Ciebie atrakcji",
+        coverImage: "aktywny-wrzesien/cover.png"
+    },
+    steps: [
+        {
+            "name": "Spacer po parku Włókniarzy",
+            "desc": "Odwiedź Park Włókniarzy, odpocznij na ławce, podziwiaj piękne kolory jesiennych drzew a dodatkowo skorzystaj z rabatu 25% na kawę w Klub Miasto.",
+            "local": {
+                "lat": 49.8090032,
+                "lng": 19.040686
+            },
+            "image": "http://images-bbdays4it-hack.s3-website.eu-central-1.amazonaws.com/jesien/1.png",
+            "type": "GPS",
+            "status": "COMPLETED"
+        },
+        {
+            "name": "Trening w parku przy ratuszu",
+            "desc": "Strefa street workout przy Ratuszu czeka na Ciebie, opublikuj swoje zdjęcie na jednej z maszyn w parku z dopiskiem #jesiennyTreningBB aby wykonac to zadanie.",
+            "local": {
+                "lat": 49.8218582,
+                "lng": 19.0483722
+            },
+            "image": "http://images-bbdays4it-hack.s3-website.eu-central-1.amazonaws.com/jesien/2.png",
+            "type": "Social",
+            "status": "COMPLETED"
+        },
+        {
+            "name": "Rowerem przez Wapienicę",
+            "desc": "Skorzystaj z ostatnich ciepłych dni i pięknej pogody za oknem. Wrzuć swoje zdjęcie z rowerem i Krzywą chatą w tle by wykonać to zadanie.",
+            "local": {
+                "lat": 49.7721221,
+                "lng": 18.9709157
+            },
+            "image": "http://images-bbdays4it-hack.s3-website.eu-central-1.amazonaws.com/jesien/3.png",
+            "type": "Social",
+            "status": "NOT"
+        },
+        {
+            "name": "Jesienny jarmark na rynku miejskim",
+            "desc": "Już w ten weekend na rynku czeka na Ciebie mnóstwo atrakcji - koncert, jarmark i pokazy lokalnych rzemieślników. Pokazując aplikację możesz dodatkowo skorzystać z 25% rabatu od ceny zakupu",
+            "local": {
+                "lat": 49.8213868,
+                "lng": 19.0403515
+            },
+            "image": "http://images-bbdays4it-hack.s3-website.eu-central-1.amazonaws.com/jesien/4.png",
+            "type": "GPS",
+            "status": "NOT"
+        },
+        {
+            "name": "Ucieknij z Escape Roomu",
+            "desc": "Jesienna deszczowa pogoda popsuła Ci plany? Nic straconego! Wszystkie bielskie escape roomy dołączają do nas tego tygodnia i oferują Tobie zniżkę 30% na wejście. Przy zakupie możesz również zeskanować QR kod i wykonać to zadanie",
+            "local": {
+                "lat": 49.8215205,
+                "lng": 19.0298284
+            },
+            "image": "http://images-bbdays4it-hack.s3-website.eu-central-1.amazonaws.com/jesien/5.png",
+            "type": "QR",
+            "status": "NOT"
+        }
+    ]
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,23 +2,27 @@ import Events from "./data/events";
 import bbdays4it from "./data/bbdays4it";
 import zadymkaJazzowa from "./data/zadymkaJazzowa";
 import szlakiemMurali from "./data/szlakiemMurali";
+import aktywnyWrzesien from "./data/aktywnyWrzesien";
 
 const EventData = {
     bbdays4it,
     zadymkaJazzowa,
-    szlakiemMurali
+    szlakiemMurali,
+    aktywnyWrzesien
 };
 
 export default {
     "GET": {
         "/": (queryStringParameters) => {
             return {
-                ping: new Date().getTime()
+                ping: new Date().getTime(),
+                params: queryStringParameters
             }
         },
         "/events": () => Events,
         "/events/bbdays4it": () => EventData.bbdays4it,
         "/events/zadymka-jazzowa": () => EventData.zadymkaJazzowa,
+        "/events/aktywny-wrzesien": () => EventData.aktywnyWrzesien,
         "/events/szlakiem-murali": () => EventData.szlakiemMurali
     },
     "POST": {
